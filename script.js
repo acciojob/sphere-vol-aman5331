8c4f5a06e567b7137afc358acf9cb17a1dc01055
Amandeep Saxena <75635658+aman5331@users.noreply.github.com>
2023-06-22 08:56:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function volume_sphere() {
     //Write your code here
+	 const radius = parseFloat(document.getElementById('radius').value);
+  const volumeField = document.getElementById('volume');
+
+  if (isNaN(radius) || radius <= 0) {
+    volumeField.value = 'Invalid radius';
+  } else {
+    const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+    volumeField.value = volume.toFixed(2);
+  }
   
 } 
 
